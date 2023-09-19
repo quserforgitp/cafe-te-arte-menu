e63ecaf7fe10306ebfdb7526218f9fa01901ca41
qAutHorp <adealumnonegligas@gmail.com>
2023-09-19 13:20:25 -0600

    detectar cuando el usuario visita desde un celular acostado

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //ELEMENTS
 const menuImage = document.getElementById('img-menu-cafe-te-arte');
 
-// FUNCTIONS
+// TRIGGERS/EVENTS
+window.addEventListener('resize',confContent);
+
+// FUNCTIONS/EVENT HANDLERS
 function
 quitLoadScreen ()
 {
@@ -9,9 +12,39 @@ quitLoadScreen ()
   setTimeout(() => {
     const loadingScreen = document.querySelector('.carga');
     loadingScreen.classList.add('desaparecer-arriba');
-  },3000)
+  },0)// cambiar a 3000
 }
+
+// 
+function 
+detectarTipoDeDispositivo ()
+{
+  // basado en user agent
+  const userAgent = navigator.userAgent;
+  const tipoDispositivo =
+  (userAgent.indexOf("Android") !== -1 || userAgent.indexOf("iPhone") !== -1)?
+  'phone': 'no-phone';
+  return tipoDispositivo;
+}
+function orientacionCelular ()
+{
+  const deviceWidth = window.innerWidth;
+  const deviceHeight = window.innerHeight;
+  const estaAcostado = (deviceWidth > deviceHeight);
+  
+  return estaAcostado ? 'acostado' : 'parado';
+}
+function confContent()
+{
+  const esCelular = detectarTipoDeDispositivo() === 'phone';
+  const estaAcostado = orientacionCelular() === 'acostado';
+  if ( esCelular )
+    if ( estaAcostado )
+      alert('Por favor orienta tu celular verticalmente para visualizar el menu de manera correcta');
+}
+
 
 
 // CALLS
 quitLoadScreen();
+confContent();
